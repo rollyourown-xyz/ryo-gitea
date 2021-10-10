7c0abfeee30f7bb757747f5bb279637750463fdb
xyzroller <xyzroller@rollyourown.xyz>
2021-10-10 18:54:19 +0200

    Added scripts

--- build-images.sh
+++ build-images.sh
@@ -2,18 +2,16 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Add additional help for software package versions defined below
-# Option "-g grav_version" as an example
 helpMessage()
 {
    echo "build-images.sh: Use packer to build images for deployment"
    echo ""
    echo "Help: build-images.sh"
-   echo "Usage: ./build-images.sh -m -n hostname -g grav_version -v version"
+   echo "Usage: ./build-images.sh -m -n hostname -g gitea_version -v version"
    echo "Flags:"
    echo -e "-m \t\t\t(Optional) Flag to also build images for modules"
    echo -e "-n hostname \t\t(Mandatory) Name of the host for which to build images"
-   echo -e "-g grav_version \t(Optional) Override default grav version to use for the webserver image, e.g. 1.7.17 (default)"
+   echo -e "-g gitea_version \t(Optional) Override default gitea version to use for the git server image, e.g. 1.15.3 (default)"
    echo -e "-v version \t\t(Mandatory) Version stamp to apply to images, e.g. 20210101-1"
    echo -e "-h \t\t\tPrint this help message"
    echo ""
@@ -28,33 +26,28 @@ errorMessage()
 }
 
 # Default software package versions
-# Add software package versions here, for example grav
-grav_version='1.7.17'
+gitea_version='1.15.3'
 
 build_modules='false'
 
-# Add additional options for software package versions defined above
-# Option "-g grav_version" as an example
 while getopts mn:g:v:h flag
 do
     case "${flag}" in
         m) build_modules='true';;
         n) hostname=${OPTARG};;
-        g) grav_version=${OPTARG};;
+        g) gitea_version=${OPTARG};;
         v) version=${OPTARG};;
         h) helpMessage ;;
         ?) errorMessage ;;
     esac
 done
 
-if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$grav_version" ]
+if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$gitea_version" ]
 then
    errorMessage
 fi
 
-# Common to almost all projects - service proxy module
-# Remove if not needed for this project
-# Build module images if -m flag is present
+# Build ryo-service-proxy module images if -m flag is present
 if [ $build_modules == 'true' ]
 then
    echo "Running build-images script for ryo-service-proxy module on "$hostname""
@@ -65,11 +58,22 @@ else
    echo ""
 fi
 
-# Project-specific image builds here..., for example
-#echo "Building webserver image on "$hostname""
-#echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"grav_version="$grav_version"\" -var \"version="$version"\" "$SCRIPT_DIR"/image-build/webserver.pkr.hcl"
-#echo ""
-#packer build -var "host_id="$hostname"" -var "grav_version="$grav_version"" -var "version="$version"" "$SCRIPT_DIR"/image-build/webserver.pkr.hcl
-#echo ""
+# Build ryo-mariadb module images if -m flag is present
+if [ $build_modules == 'true' ]
+then
+   echo "Running build-images script for ryo-mariadb module on "$hostname""
+   echo ""
+   "$SCRIPT_DIR"/../ryo-mariadb/build-images.sh -n "$hostname" -v "$version"
+else
+   echo "Skipping image build for modules"
+   echo ""
+fi
+
+# Build project images
+echo "Building git server image on "$hostname""
+echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version="$version"\" -var \"gitea_version="$gitea_version"\" "$SCRIPT_DIR"/image-build/gitea.pkr.hcl"
+echo ""
+packer build -var "host_id="$hostname"" -var "version="$version"" -var "gitea_version="$gitea_version"" "$SCRIPT_DIR"/image-build/gitea.pkr.hcl
+echo ""
 
 echo "Completed"
