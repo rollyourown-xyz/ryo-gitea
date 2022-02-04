3b12976c8787ab4f099dda0c0f09ce4a0b4e7b63
xyzroller <xyzroller@rollyourown.xyz>
2022-02-04 16:18:21 +0100

    Separate ACL deploy from use-backend deploy

--- project-deployment/05-deploy-ingress-proxy-configuration-gitea.tf
+++ project-deployment/05-deploy-ingress-proxy-configuration-gitea.tf
@@ -21,7 +21,7 @@ module "deploy-gitea-ingress-proxy-backend-service" {
   non_ssl_backend_services     = [ "gitea-http" ]
 }
 
-module "deploy-gitea-http-ingress-proxy-configuration" {
+module "deploy-gitea-http-ingress-proxy-acl-configuration" {
   source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
 
   depends_on = [ module.deploy-gitea-ingress-proxy-backend-service ]
@@ -29,6 +29,13 @@ module "deploy-gitea-http-ingress-proxy-configuration" {
   ingress-proxy_host_only_acls = {
     host-gitea-http = {host = local.project_domain_name}
   }
+
+}
+
+module "deploy-gitea-http-ingress-proxy-use-backend-configuration" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-gitea-ingress-proxy-backend-service, module.deploy-gitea-http-ingress-proxy-acl-configuration ]
 
   ingress-proxy_acl_use-backends = {
     host-gitea-http = {backend_service = "gitea-http"}
