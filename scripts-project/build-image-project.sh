0e546c6bc0aa8b57814661b06f308668ba391fa2
xyzroller <xyzroller@rollyourown.xyz>
2022-02-04 07:33:23 +0100

    Bump Gitea version to 1.15.11

--- scripts-project/build-image-project.sh
+++ scripts-project/build-image-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default project software versions
-gitea_version="1.15.10"
+gitea_version="1.15.11"
 
 # Script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
